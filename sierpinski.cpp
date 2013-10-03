7156f518e1ed0ec91886c258405a4ccf8cadb2bc
Sebastian Monnin <monninse@miamioh.edu>
2013-10-02 20:26:16 -0400

    bugs fixed

--- sierpinski.cpp
+++ sierpinski.cpp
@@ -60,7 +60,7 @@ void doTriangle(int sideLength)
 	grid[sideLength*sideLength] = '\0';
 
 	int i;
-	for(i=0; i < sideLength*sideLength; i++);
+	for(i=0; i < sideLength*sideLength; i++)
 	{
 		grid[i] = ' ';
 	}
@@ -76,7 +76,7 @@ void doTriangle(int sideLength)
 	grid[((int)p2y)*sideLength + (int)p2x] = '8';
 
 	//The third corner is in the middle of the top
-	double p3x = sideLength - 1;
+	double p3x = sideLength/2;
 	double p3y = 0;
 	grid[((int)p3y)*sideLength + (int)p3x] = '8';
 
@@ -100,6 +100,7 @@ void doTriangle(int sideLength)
 		case 0:
 			curx = (curx + p1x)/2.0;
 			cury = (cury + p1y)/2.0;
+			break;
 		case 1:
 			curx = (curx + p2x)/2.0;
 			cury = (cury + p2y)/2.0;
